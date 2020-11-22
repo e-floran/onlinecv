04fbb77d7d73db13c9cbc768a45a4a8d11b021a3
e-floran <e-floran@hotmail.fr>
2020-11-22 20:46:32 +0100

    working on header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import "../style/header.css";
+
+function Header(){
+    return(
+        <header>
+            <h1>Floran Eiclies</h1>
+            <Navbar />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Formations</li>
+                <li>Expériences</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
