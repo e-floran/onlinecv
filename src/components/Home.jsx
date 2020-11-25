ab7d558982a47c037c84e3a870e5e3c435f79fb0
e-floran <e-floran@hotmail.fr>
2020-11-25 14:19:38 +0100

    added picture and description in know me

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import Description from "./Description";
+import KnowMe from "./KnowMe";
 import '../style/home.css';
 function Home(){
     return(
         <div className="homeContainer">
             <Description />
+            <KnowMe />
         </div>
     )
 }
