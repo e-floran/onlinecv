8e688a55e45459a1b11e3b269da861ad00501f4a
e-floran <e-floran@hotmail.fr>
2020-12-01 22:10:01 +0100

    erased details

--- src/components/education/Journalism.jsx
+++ src/components/education/Journalism.jsx
@@ -11,21 +11,9 @@ function Journalism(){
             </article>
             <article className="course">
                 <h5 className="courseTitle"><span className="courseDot"/>Master en journalisme et communication économique, Faculté d'économie appliquée, Université Aix-Marseille III, 2010-2011</h5>
-                <div className="detailsContainer">
-                    <p className="soloDetail">Enquête</p>
-                    <p className="soloDetail">Écriture</p>
-                    <p className="soloDetail">Relecture</p>
-                    <p className="soloDetail">Maquette</p>
-                    <p className="soloDetail">Prise d'images</p>
-                </div>
             </article>
             <article className="course">
                 <h5 className="courseTitle"><span className="courseDot"/>Diplôme universitaire en journalisme et communication web, Faculté d'économie appliquée, Université Aix-Marseille III, 2010-2011</h5>
-                <div className="detailsContainer">
-                    <p className="soloDetail">Formatage</p>
-                    <p className="soloDetail">Référencement</p>
-                    <p className="soloDetail">Montage vidéo</p>
-                </div>
             </article>
         </section>
     )
--- src/components/education/WebDev.jsx
+++ src/components/education/WebDev.jsx
@@ -11,15 +11,6 @@ function WebDev(){
             </article>
             <article className="course">
                 <h5 className="courseTitle"><span className="courseDot"/>Développeur web, <a href="https://www.wildcodeschool.com/fr-FR/formations/developpeur-web">Wild code school</a>, campus remote, 2020-2021 (en cours)</h5>
-                <div className="detailsContainer">
-                    <p className="soloDetail">Intégration</p>
-                    <p className="soloDetail">Algorithmie</p>
-                    <p className="soloDetail">JavaScript</p>
-                    <p className="soloDetail">Scrum</p>
-                    <p className="soloDetail">React</p>
-                    <p className="soloDetail">SQL</p>
-                    <p className="soloDetail">Node.js</p>
-                </div>
             </article>
         </section>
     )
