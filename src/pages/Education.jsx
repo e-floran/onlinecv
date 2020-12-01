33f8d0aaa2ce74866ed666ff8b26b7a1f7080ad2
e-floran <e-floran@hotmail.fr>
2020-12-01 22:03:51 +0100

    added journalism

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,10 +1,12 @@
 import WebDev from "../components/education/WebDev";
+import Journalism from "../components/education/Journalism";
 import "../style/education.css";
 function Education(){
     return(
         <div className="pageContainer">
             <p className="pageIntro">Après une formation universitaire initiale en économie, puis en journalisme, j'apprends aujourd'hui un nouveau domaine de compétences à l'aide d'une formation en développement web.</p>
             <WebDev />
+            <Journalism />
         </div>
     )
 };
