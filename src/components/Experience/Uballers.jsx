3d48920487111df128f768d20a2fba342d4aee73
e-floran <e-floran@hotmail.fr>
2021-02-24 20:30:26 +0100

    updated home

--- src/components/Experience/Uballers.jsx
+++ src/components/Experience/Uballers.jsx
@@ -2,7 +2,7 @@ function Uballers(){
     return(
         <article className="project">
             <h5 className="projectTitle">Uballers <span><span className="miniPulsar" style={{animation: `pulsed 2s linear 0.5s infinite`}}/><span className="miniPulsar" style={{animation: `pulsed 2s linear 0.75s infinite`}}/><span className="miniPulsar" style={{animation: `pulsed 2s linear 1s infinite`}}/></span></h5>
-            <h6 className="projectDate">Depuis mars 2021</h6>
+            <h6 className="projectDate">Télétravail, depuis mars 2021</h6>
             <p>Pour lancer ma carrière de développeur web, j'ai la chance de pouvoir participer à ce projet ambitieux : <a href="https://uballers.com/" title="Site de Uballers">Uballers</a></p>
             <p>Étant passionné de basket, l'univers de l'entreprise est évidemment un vrai plus pour moi.</p>
             <div className="technoContainer">
--- src/components/home/Description.jsx
+++ src/components/home/Description.jsx
@@ -7,9 +7,9 @@ function Description(){
                 </div>
                 <h3>Mon parcours</h3>
             </article>
-            <p>Après plusieurs expériences réussies dans la presse et après avoir touché à tous les métiers qu'elle a à offrir, je me lance un nouveau défi, tourné vers l'avenir, en devenant développeur web.</p>
-            <p>Depuis le 11 septembre dernier et jusqu'au 12 février, je suis une formation "Développeur web" sur le campus remote de la Wild code school. Elle me permettra d'obtenir le titre professionnel "Développeur web et web mobile", équivalent bac+2, inscrit au RNCP.</p>
-            <p>Pour lancer ma nouvelle carrière, je suis actuellement à la recherche d'un stage, en télétravail ou en présentiel, pour février.</p>
+            <p>Je suis actuellement développeur web chez Uballers. J'apporte une vraie passion pour le développement mais aussi un profil atypique enrichi par ma première vie professionnelle.</p>
+            <p>En effet, après plusieurs expériences réussies dans la presse et après avoir touché à tous les métiers qu'elle a à offrir, je me suis lancé un nouveau défi, tourné vers l'avenir, en devenant développeur.</p>
+            <p>Pour démarrer cette nouvelle carrière, entre 2020 et 2021, j'ai suivi une formation "Développeur web" sur le campus remote de la Wild code school.</p>
         </section>
     )
 };
